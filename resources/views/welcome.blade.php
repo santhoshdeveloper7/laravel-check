1cf85934161d08e170a77ae25e33c34ef37ffbaa
santhoshdeveloper7 <developer@haodapayments.com>
2025-11-13 11:29:30 +0530

    welcome update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
 </head>
 <body>
     <h1>Welcome to My Laravel Project 🎉</h1>
-    <p>This is my first Laravel page. asdas sd</p>
+    <p>This is my first Laravel page.</p>
+    <p>Hello CI/CD Pipeline</p>
 </body>
 </html>
